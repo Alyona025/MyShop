6b460d16331e71af9eb92b69edfadc3af9dfe3d9
Алёна <gloria900dfg@gmail.com>
2025-07-09 19:12:50 +0900

    Добавлена проверка на наличие продукта по ID; Добавление графы: прибыль магазина в рублях.

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -17,8 +17,11 @@ namespace MyShop
             shop.products.Add(new Product("Сок", 130, 3));
             shop.products.Add(new Product("Пельмени", 100, 5));
 
-            while (true)
+            int profit = 0;
+            while (true) //начинается цикл
             {
+                Console.WriteLine($"Прибыль магазина {shop.Name}: {profit} рублей");
+                Console.WriteLine();
                 foreach (Product product in shop.products)
                 {
                     Console.WriteLine($"{product.Title} цена - {product.Price} рублей, {product.Count} штук, ID: {product.Id}");
@@ -28,11 +31,15 @@ namespace MyShop
                 Console.Write("Введите количество: ");
                 int count = int.Parse(Console.ReadLine());
 
+                bool found = false; // для проверки наличия товара 
+
                 foreach (Product product in shop.products)
                 {
                     if (id == product.Id)
                     {
-                        
+
+                    found = true;
+
                         if (count < 0)
                         {
                             Console.WriteLine("Введите положительное число");
@@ -45,16 +52,20 @@ namespace MyShop
                         }
                         else
                         {
+                            profit += product.Price * count;
                             product.Count -= count;
                             break;
                         }
-                        
                     }
-
-                    
+                   
+                }
+                if (!found)
+                {
+                    Console.WriteLine("У магазина нет товара с таким ID");
+                    Console.ReadLine();
                 }
                 Console.Clear();
-            }
+            } //заканчивается цикл while.
 
         }
     }
