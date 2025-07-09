7faf1972d96deda392ba1d1189a4a9d7a77c600d
Алёна <gloria900dfg@gmail.com>
2025-07-09 14:21:07 +0900

    Добавлена возможность добавлять товар и добавила ID.

--- MyShop/Product.cs
+++ MyShop/Product.cs
@@ -10,12 +10,16 @@ namespace MyShop
     {
         public string Title;
         public int Price;
+        public int Count;
+        public int Id;
+        private static int NextId = 0;
 
-        public Product(string title, int price)
+        public Product(string title, int price, int count)
         {
             Title = title;
             Price = price;
-
+            Count = count;
+            Id = NextId++;
         }
 
 
--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -10,12 +10,51 @@ namespace MyShop
     {
         static void Main()
         {
-            Shop s = new Shop("Пятёрочка");
+            Shop shop = new Shop("Пятёрочка");
 
-            shop.products.Add(new Product("Хлеб", 25));
-            shop.products.Add(new Product("Молоко", 80));
-            shop.products.Add(new Product("Сок", 130));
-            shop.products.Add(new Product("Пельмени", 100));
+            shop.products.Add(new Product("Хлеб", 25, 10));
+            shop.products.Add(new Product("Молоко", 80, 15));
+            shop.products.Add(new Product("Сок", 130, 3));
+            shop.products.Add(new Product("Пельмени", 100, 5));
+
+            while (true)
+            {
+                foreach (Product product in shop.products)
+                {
+                    Console.WriteLine($"{product.Title} цена - {product.Price} рублей, {product.Count} штук, ID: {product.Id}");
+                }
+                Console.Write("Введите ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Введите количество: ");
+                int count = int.Parse(Console.ReadLine());
+
+                foreach (Product product in shop.products)
+                {
+                    if (id == product.Id)
+                    {
+                        
+                        if (count < 0)
+                        {
+                            Console.WriteLine("Введите положительное число");
+                            Console.ReadLine();
+                        }
+                        else if (count > product.Count)
+                        {
+                            Console.WriteLine("Нет столько товаров");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            product.Count -= count;
+                            break;
+                        }
+                        
+                    }
+
+                    
+                }
+                Console.Clear();
+            }
 
         }
     }
